d973366fb9c4b9e4e0110a73c9af3f0926dd24fb
peace <edwardpeacendidiamaka@gmail.com>
2022-09-10 12:54:56 -0700

    My fourth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- *  *main- Serves as the entry point for the program
- *   *
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
- *     */
-int main(void)
+ * main - entry point
+ *
+ *
+ * Return: 0
+ */
+int main (void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
